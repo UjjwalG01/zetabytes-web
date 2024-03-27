fdc473ef0830330ce5c3a6498eecfc4d54d185c1
Ujjwal Giri <ug.online.st@gmail.com>
2024-03-27 23:27:53 +0545

    Database integration and user register

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server"
 
 import * as z from "zod";
+import bcrypt from "bcrypt"
+
+import { db } from "@/lib/db";
 
 import { RegisterSchema } from "@/schemas";
 
@@ -12,5 +15,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields!" }
     }
 
-    return { success: "Registration successful!" }
+    const { email, name, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email already exists" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    // TODO: send verification email
+
+    return { success: "User created successfully!" }
 }
