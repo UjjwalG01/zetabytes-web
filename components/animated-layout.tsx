f207c783da8524a564251cd62fc1b4ec1355d132
Ujjwal Giri <ug.online.st@gmail.com>
2024-03-31 23:16:26 +0545

    statrups route update

--- components/animated-layout.tsx
+++ components/animated-layout.tsx
@@ -12,12 +12,7 @@ export const AnimatedLayout = ({ children }: AnimatedLayoputProps) => {
   return (
     <>
       <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 1, y: 15 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 15 }}
-          transition={{ delay: 0.25 }}
-        >
+        <motion.div>
           <Header />
           {children}
           <Footer />
