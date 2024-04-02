459f123f860fc8bb2840f75ad729e7f017988304
Ujjwal Giri <ug.online.st@gmail.com>
2024-04-02 22:30:42 +0545

    configuration

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,19 +1,10 @@
 import { UserRole } from "@prisma/client";
 import NextAuth, { DefaultSession } from "next-auth"
 
-// declare module "next-auth" {
-//     interface Session {
-//         user: {
-//             role: string;
-//         }
-//     }
-// }
-
 export type ExtendedUser = DefaultSession["user"] & {
     // You can add any custom fields here
     role: UserRole
 };
-
 declare module "next-auth" {
     interface Session {
         user: ExtendedUser
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      use: "html-loader",
+    });
+    config.externals = [...config.externals, "bcrypt"];
+    return config;
+  },
+  // webpack: (config) => {
+  //   config.externals = [...config.externals, "bcrypt"];
+  //   return config;
+  // },
+};
 
 export default nextConfig;
